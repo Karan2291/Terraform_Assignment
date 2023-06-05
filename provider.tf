2e46b65a1bddff007be42fdd0f9dc0338708e806
Karan Bajaj <karan.b@sigmoidanalytics.com>
2023-06-05 12:07:00 +0530

    uploading varaibles.tf file

--- provider.tf
+++ provider.tf
@@ -11,6 +11,8 @@ terraform {
 # Access Key and Secret Key is passed as an environmental variable
 provider "aws" {
   region = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
 }
 
 
