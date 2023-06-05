67907e4c740d2f8c9532af2af1d1d6968cb27c03
Karan Bajaj <karan.b@sigmoidanalytics.com>
2023-06-05 11:56:38 +0530

    uploading terraform assignment

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider 
+# Access Key and Secret Key is passed as an environmental variable
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+ 
